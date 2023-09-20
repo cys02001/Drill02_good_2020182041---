507ca2adc7f8ce010d0aa894a5bd3694b803cbeb
yunseochoi(home) <jamie0730@naver.com>
2023-09-20 18:08:02 +0900

    make bottom line

--- character_run.py
+++ character_run.py
@@ -21,11 +21,19 @@ def run_circle():
 
 def run_rectangle():
     print('RECTANGLE')
+
+    #bottom line
+    for x in range(50, 750+1, 10):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,90)
+        delay(0.01)
+    
     pass
 
 
 while True:
-    run_circle()
+  #  run_circle()
     run_rectangle()
     break
 
